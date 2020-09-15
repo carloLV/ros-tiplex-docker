df0ddf8e6be6bb57837a7d46295ea1e966f344eb
carloLV <carlo.laviola@gmail.com>
2020-09-15 11:45:48 +0200

    Creating ground for melodic
    - Created Dockerfile with new packages
    - Added Tiago simulation to run on Gazebo
    - Updated issues in scripts

--- data/env_no_make.sh
+++ data/env_no_make.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 cd ~ && \
-    mkdir -p ./catkin_ws/src && \
-    cd catkin_ws/src && \
+    mkdir -p ./tiago_ws/src && \
+    cd tiago_ws/src && \
     ln -s /app/tbot_testing_env && \
     ln -s /app/robot_knowledge_base && \
     cp /app/start_node* ../ && \
     chmod +x ../start_node* && \
     cd robot_knowledge_base/src/ && \
     chmod +x back_robot_side.py back_planner_side.py && \
-    cd ~/catkin_ws && \
-    catkin_make
+    cd ~/tiago_ws
